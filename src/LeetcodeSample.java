4fef1522a97483949f4c6881d0db5d0e08249a96
Ge Ding <447405890@qq.com>
2021-04-09 23:59:08 -0400

    daily update

--- src/LeetcodeSample.java
+++ src/LeetcodeSample.java
@@ -922,15 +922,45 @@ public class LeetcodeSample {
     //Input: s = "egg", t = "add"
     //Output: true
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Integer> s_map = new HashMap<Character, Integer>();
-        HashMap<Character,Integer> t_map = new HashMap<Character, Integer>();
-        char[] s_arr = s.toCharArray();
-        char[] t_arr = t.toCharArray();
-
-        for (int i = 0; i< s_arr.length; i++) {
-            s_map.get()
+        int[] arr = new int[512];
+        for (int i = 0; i< arr.length; i++) arr[i] = 0;
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(arr[s.charAt(i)] + " / " +  arr[t.charAt(i)+256]);
+            if (arr[s.charAt(i)] != arr[t.charAt(i)+256]) return false;
+            arr[i] += 1;
+            arr[i+256] += 1;
         }
+        return true;
     }
+
+    //647-Palindromic Substrings
+    //Given a string, your task is to count how many palindromic substrings in this string.
+    //Input: "abc"
+    //Output: 3
+    public int countSubstrings(String s) {
+        int count = 0;
+
+        return count;
+    }
+
+    //696-Count Binary Substrings
+    //Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+    //Input: "00110011"
+    //Output: 6
+    public int countBinarySubstrings(String s) {
+        int[] counts = new int[s.length()];
+        int t = 0;
+        counts[0] = 1;
+        for (int i = 1; i < s.length();i++) {
+            if (s.charAt(i-1) == s.charAt(i)) counts[t]++;
+            else counts[++t] = 1;
+        }
+        int ans = 0;
+        for (int i = 1; i <= t; i++) ans += Math.min(counts[i],counts[i-1]);
+        return ans;
+    }
+
+    
 
 
 }
--- src/TypicalString.java
+++ src/TypicalString.java
@@ -34,4 +34,37 @@ public class TypicalString {
         return set.isEmpty() ? count:count+1;
     }
 
+    //205-Isomorphic Strings
+    //Given two strings s and t, determine if they are isomorphic.
+    //Input: s = "egg", t = "add"
+    //Output: true
+    public boolean isIsomorphic(String s, String t) {
+        int[] arr = new int[512];
+        for (int i = 0; i< arr.length; i++) arr[i] = 0;
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(arr[s.charAt(i)] + " / " +  arr[t.charAt(i)+256]);
+            if (arr[s.charAt(i)] != arr[t.charAt(i)+256]) return false;
+            arr[i] += 1;
+            arr[i+256] += 1;
+        }
+        return true;
+    }
+
+    //696-Count Binary Substrings
+    //Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+    //Input: "00110011"
+    //Output: 6
+    public int countBinarySubstrings(String s) {
+        int[] counts = new int[s.length()];
+        int t = 0;
+        counts[0] = 1;
+        for (int i = 1; i < s.length();i++) {
+            if (s.charAt(i-1) == s.charAt(i)) counts[t]++;
+            else counts[++t] = 1;
+        }
+        int ans = 0;
+        for (int i = 1; i <= t; i++) ans += Math.min(counts[i],counts[i-1]);
+        return ans;
+    }
+
 }
